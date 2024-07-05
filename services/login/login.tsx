4679d653ab9961e8b4363b8b64ef7e83339de223
Luis Fernando Paganini <luisfernando_paganini@hotmail.com>
2024-07-05 19:37:29 -0300

    fix: refresh token

--- services/login/login.tsx
+++ services/login/login.tsx
@@ -33,26 +33,24 @@ export const refreshAccessToken = async (refreshToken: string, token: string): P
     const response = await api.post(`login/refresh`, { refreshToken, accessToken: token });
 
     return response.data;
-  };
+};
 
 export const setupTokenRefresh = () => {
-    const { accessToken, setAccessToken, refreshToken, setRefreshToken } = useTokenStore.getState();
-    
-    if (!refreshToken || !accessToken) return;
-  
-    // setInterval(async () => {
-    //   try {
-    //     console.log(refreshToken)
-    //     console.log(accessToken)
-    //     const tokens = await refreshAccessToken(refreshToken, accessToken);
-    //     if (!tokens.success) throw new Error('Erro ao renovar o token');
+    const { clearTokens } = useTokenStore.getState();
+    setInterval(async () => {
+        try {
+            const { accessToken, setAccessToken, refreshToken, setRefreshToken } = useTokenStore.getState();
 
-    //     clearTokens()
-    //     setAccessToken(tokens.accessToken);
-    //     setRefreshToken(tokens.refreshToken);
+            if (!refreshToken || !accessToken) return;
 
-    //   } catch (error) {
-    //     console.error('Erro ao renovar o token:', error);
-    //   }
-    // }, 15000); // Renova o token a cada 15 minutos
-  };
+            const tokens = await refreshAccessToken(refreshToken, accessToken);
+            if (!tokens.success) throw new Error('Erro ao renovar o token');
+
+            setAccessToken(tokens.accessToken);
+            setRefreshToken(tokens.refreshToken);
+        } catch (error) {
+            console.error('Erro ao renovar o token:', error);
+            clearTokens();
+        }
+    }, 1000 * 60 * 15);
+};
