6bfaff37c0ed48f3a3a26344594417cbe5f4fd3b
Luis Fernando Paganini <luisfernando_paganini@hotmail.com>
2024-07-08 22:09:48 -0300

    feat: ISnackInterface

--- interfaces/Snack/ISnackInterface.tsx
+++ interfaces/Snack/ISnackInterface.tsx
@@ -1,12 +1,12 @@
 import ISnackListInterface from "./ISnackListInterface";
 
 export default interface ISnackInterface {
+    date: Date;
     carbohydrates: number;
     calories: number;
     colesterol: number;
     protein: number;
     others: number;
     totalCalories: number;
-    date: Date;
     snacks: ISnackListInterface[];
 }
